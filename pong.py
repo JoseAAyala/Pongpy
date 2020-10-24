321b7d0f6a0f99b4266a50c2e0fb41c1b03c8500
Jose Ayala <ayala.brassea@gmail.com>
2020-10-23 17:58:16 -0700

    dibujo

--- pong.py
+++ pong.py
@@ -1,2 +1,32 @@
-print("hola mundo")
-print("otro")
+import pygame
+
+pygame.init()
+clock = pygame.time.Clock()
+
+screen_width = 1080
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Pong")
+
+ball = pygame.Rect(screen_width/2-11, screen_height/2-11, 22, 22)
+player = pygame.Rect(screen_width-20, screen_height/2-60, 10, 120)
+enemy = pygame.Rect(10, screen_height/2-60, 10, 120)
+
+
+bgColor = (25,25,25)
+objectColor = (115,115,115)
+
+while True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit()
+
+	screen.fill(bgColor)
+	pygame.draw.rect(screen, objectColor, player)
+	pygame.draw.rect(screen, objectColor, enemy)
+	pygame.draw.ellipse(screen, objectColor, ball)
+	pygame.draw.aaline(screen, objectColor, (screen_width/2,0), (screen_width/2,screen_height))
+
+	pygame.display.flip()
+	clock.tick(60)
